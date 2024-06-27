a685b439f120ec09566bff6c8f0786898f0d1321
JavierCana <javiercanaalvaro@gmail.com>
2024-06-28 00:34:18 +0200

    Checkpoint: funcionan normales pero no freebets

--- Sistema.java
+++ Sistema.java
@@ -17,12 +17,19 @@ public class Sistema {
             double[] cuotas = new double[casos];
             boolean[] isFreebet = new boolean[casos];
             // preguntar por cuotas
-            for(int i=0;i<casos;i++){
-                System.out.println("Cuota numero " +i);
-                cuotas[i] = input.nextInt();
-                System.out.println("Usar freebets (S/N)?");
-                isFreebet[i] = Character.toUpperCase(input.next().charAt(0)) == 'S';
+            System.out.println("Indique si va a usar freebets en alguna cuota (S/N)");
+            System.out.println("seguido de las cuotas de los casos que hay p.e: N 2,0 5,5 12");
+            boolean usarFree = Character.toUpperCase(input.next().charAt(0)) == 'S';
+            //Obtención de las cuotas
+            for(int i=0;i<casos;i++){ cuotas[i] = input.nextDouble(); }
+
+            //! Ver en cual queremos usar freebets, en ellas restamos 1 a la cuota (perdemos el importe de la free al ganar)
+            if(usarFree){
+                System.out.println("Indique en cuales cuotas va a usar freebets escribiendo S/N");
+                System.out.println("ejemplo: si las cuotas son 1,8 y 1,91 y no queremos usar freebets en ninguna sería 'N N'");
+                for(int i=0;i<casos;i++){ isFreebet[i] = Character.toUpperCase(input.next().charAt(0)) == 'S'; }
             }
+
             // Preguntar la operacion a realizar
             System.out.println("Seleccione la operación a realizar:");
             System.out.println("0: Surebet (ganar igual en ambos lados) con cantidad fija a una cuota");
@@ -30,7 +37,8 @@ public class Sistema {
             int accion = input.nextInt();
             switch (accion) {
                 case SUREBET:
-                    System.out.println("Indica la cantidad fija a la primera cuota p.e 2,10");
+                    System.out.println("Indica la cantidad fija de dinero a la primera cuota p.e 10,0");
+                    // realizarSurebet(cuotas,isFreebet,input.nextDouble());
                     realizarSurebet(cuotas,isFreebet,input.nextDouble());
                     break;
                 case FIN:
@@ -41,24 +49,44 @@ public class Sistema {
             }
         }
     }
-
-    public static void realizarSurebet(double[] cuotas, boolean[] isFreebet, double betFijaCuota1){
-        double[] apuestas = new double[cuotas.length]; //tamaño=casos 
+    // *****************************************************************************************
+    // *                              [COMENTARIO BONITO]                                      *
+    // *****************************************************************************************
+    
+    // *****************************************************************************************
+    public static void realizarSurebet(double[] cuotas,boolean[] isFreebet, double betFijaCuota1){
+        double sumInversasCuotas = 1/cuotas[0];
+        double apuestaTotal= isFreebet[0] ? 0.0 : betFijaCuota1;
+        double apuestaTotalFreebets = isFreebet[0] ? betFijaCuota1 : 0.0;
+        double[] apuestas = new double[cuotas.length];
+        // Realizar e imprimir calculos
         apuestas[0] = betFijaCuota1;
-        double sumProbabilidades = 0.0;
-        for(double cuota : cuotas){ sumProbabilidades += 1/cuota; }
-        // Calcular el resto de apuestas
-        double sumaApuestasRestantes = betFijaCuota1 * (1 - 1 / cuotas[0]) / sumProbabilidades;
-        System.out.printf("Apuesta indicada a la cuota %d: %.2f\n",0,betFijaCuota1);
-        double apuestaTotal = betFijaCuota1;
         for (int i = 1; i < cuotas.length; i++) {
-            apuestas[i] = sumaApuestasRestantes / (1 / cuotas[i]);  
-            apuestaTotal+=apuestas[i];
+            if(isFreebet[i]){
+                apuestas[i] = (betFijaCuota1 * cuotas[0]) / (cuotas[i]-1);
+                apuestaTotalFreebets+=apuestas[i];
+            }
+            else{
+                apuestas[i] = (betFijaCuota1 * cuotas[0]) / cuotas[i];
+                apuestaTotal += apuestas[i];
+            }
+            sumInversasCuotas += 1/cuotas[i];
         }
-        for (int i=0;i<apuestas.length;i++){
-            double ganancia = apuestas[i]*cuotas[i] - apuestaTotal;
-            System.out.printf("Apuesta a hacer a la cuota %d: %.2f, con beneficio neto %2.f\n",i,apuestas[i],ganancia);
-        }
+        imprimirMensaje(apuestaTotal,apuestaTotalFreebets,isFreebet,apuestas,cuotas);
+        if(sumInversasCuotas < 1){ System.out.println("!!!! ES UNA SUREBET, SI NO HAY LIMITES NO USES FREEBETS Y APUESTA FUERTE (A NO SER QUE QUIERAS BLANQUEAR) !!!!");}
     }
-
+    // *****************************************************************************************
+    // *                              imprimirMensaje                                          *
+    // *****************************************************************************************
+    // Imprime las apuestas a realizar indicando el monto total gastado, beneficios, freebets usas
+    // *****************************************************************************************
+    private static void imprimirMensaje(double apuestaDinero, double apuestaFree, boolean[]isFreebet, double[]apuestas,double[]cuotas){
+        System.out.println("----------------------");
+        System.out.printf("Apuesta Total de dinero: %.2f y de FREEBETS %.2f\n",apuestaDinero,apuestaFree);
+        for (int i = 0; i < cuotas.length; i++){
+            String resultado = isFreebet[i] ? "Apostar a cuota %.2f cantidad %.2f con profit %.3f\n" : "Apostar a cuota %.2f cantidad (de FREEBETS) %.2f con profit %.3f\n";
+            System.out.printf(resultado, cuotas[i], apuestas[i], apuestas[i] * cuotas[i] - apuestaDinero);
+        }
+        System.out.println("----------------------");
+    }
 }
